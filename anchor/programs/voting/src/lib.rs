7c7eb6ea21d2b4ecf4848cf9e97ffd990cf4098d
preeeetham <spreethamkumar5@gmail.com>
2025-04-10 09:22:12 +0530

    modified contracts

--- anchor/programs/voting/src/lib.rs
+++ anchor/programs/voting/src/lib.rs
@@ -25,17 +25,28 @@ pub mod voting {
 
     pub fn initialize_candidate(ctx: Context<InitializeCandidate>, 
                                 candidate_name: String,
-                                _poll_id: u64
+                                poll_id: u64
                             ) -> Result<()> {
         let candidate = &mut ctx.accounts.candidate;
         candidate.candidate_name = candidate_name;
+        candidate.poll_id = poll_id;
         candidate.candidate_votes = 0;
         Ok(())
     }
 
     pub fn vote(ctx: Context<Vote>, _candidate_name: String, _poll_id: u64) -> Result<()> {
         let candidate = &mut ctx.accounts.candidate;
-        candidate.candidate_votes += 1;
+        let voter_record = &mut ctx.accounts.voter_record;
+
+        if voter_record.has_voted {
+          return Err(error!(Error::AlreadyVoted));
+      }
+
+      voter_record.voter = ctx.accounts.signer.key();
+      voter_record.poll_id = _poll_id;
+      voter_record.has_voted = true;
+
+      candidate.candidate_votes += 1;
 
         msg!("Voted for candidate: {}", candidate.candidate_name);
         msg!("Votes: {}", candidate.candidate_votes);
@@ -62,6 +73,14 @@ pub struct Vote<'info> {
       bump
     )]
     pub candidate: Account<'info, Candidate>,
+    #[account(
+      init_if_needed, // this prevents from creating account if it already exists
+      payer = signer,
+      space = 8 + VoterRecord::INIT_SPACE,
+      seeds = [poll_id.to_le_bytes().as_ref(), signer.key().as_ref()],
+      bump
+    )]
+    pub voter_record : Account<'info,VoterRecord>,
 
     pub system_program: Program<'info, System>,
 }
@@ -97,6 +116,7 @@ pub struct Candidate {
     #[max_len(32)]
     pub candidate_name: String,
     pub candidate_votes: u64,
+    pub poll_id: u64,
 }
 
 #[derive(Accounts)]
@@ -124,4 +144,18 @@ pub struct Poll {
     pub poll_start: u64,
     pub poll_end: u64,
     pub candidate_amount: u64,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct VoterRecord {
+    pub voter: Pubkey,
+    pub poll_id: u64,
+    pub has_voted: bool,
+}
+
+#[error_code]
+pub enum Error {
+    #[msg("Voter has already cast a vote in this poll")]
+    AlreadyVoted,
 }
